b2e840152fc0b9dbe4d55de84a3a577afa4e11cd
Agustya Pratama <agustyapratama123@gmail.com>
2025-05-10 21:26:08 +0700

    delete some unusefull file

--- public/templates/resto-dashboard-template.php
+++
@@ -1,9 +0,0 @@
-<div class="dashboard-nav">
-	<a href="#" data-page="home">🏠 Beranda</a>
-	<a href="#" data-page="orders">🧾 Pesanan</a>
-	<a href="#" data-page="profile">👤 Profil</a>
-</div>
-
-<div id="dashboard-app">
-	<!-- Konten AJAX akan ditampilkan di sini -->
-</div>
--- public/views/dashboard/home.php
+++
@@ -1,2 +0,0 @@
-<h2>Dashboard Resto - Home</h2>
-<p>Selamat datang di dashboard admin resto!</p>
